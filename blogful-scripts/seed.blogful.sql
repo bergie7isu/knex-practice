5aa3f8203437b4cf93aa14d185ce10eba9704cf0
Mark Bergstrom <markbergstrom@gmail.com>
2020-01-14 13:07:55 -0600

    blogful testing setup

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Fish tricks', now() - '21 days'::INTERVAL, 'Fish tricks Fish tricks Fish tricks Fish tricks'),
+    ('Not Dogs', now() - '20 days'::INTERVAL, 'Not Dogs Not Dogs Not Dogs Not Dogs'),
+    ('Bluffalo Wings', now() - '19 days'::INTERVAL, 'Bluffalo Wings Bluffalo Wings Bluffalo Wings Bluffalo Wings'),
+    ('SubstiTuna Salad', now() - '18 days'::INTERVAL, 'SubstiTuna Salad SubstiTuna Salad SubstiTuna Salad SubstiTuna Salad'),
+    ('Tofurkey', now() - '17 days'::INTERVAL, 'Tofurkey Tofurkey Tofurkey Tofurkey'),
+    ('Pretenderloins', now() - '16 days'::INTERVAL, 'Pretenderloins Pretenderloins Pretenderloins Pretenderloins'),
+    ('Steak-believe', now() - '15 days'::INTERVAL, 'Steak-believe Steak-believe Steak-believe Steak-believe'),
+    ('Kale Seitan', now() - '14 days'::INTERVAL, 'Kale Seitan Kale Seitan Kale Seitan Kale Seitan'),
+    ('NoBull Burger', now() - '13 days'::INTERVAL, 'NoBull Burger NoBull Burger NoBull Burger NoBull Burger'),
+    ('Turnip the Beet', now() - '12 days'::INTERVAL, 'Turnip the Beet Turnip the Beet Turnip the Beet Turnip the Beet'),
+    ('Mascarphony', now() - '11 days'::INTERVAL, 'Mascarphony Mascarphony Mascarphony Mascarphony'),
+    ('Burgatory', now() - '10 days'::INTERVAL, 'Burgatory Burgatory Burgatory Burgatory'),
+    ('Sleight of Ham', now() - '9 days'::INTERVAL, 'Sleight of Ham Sleight of Ham Sleight of Ham Sleight of Ham'),
+    ('Antichovies', now() - '8 days'::INTERVAL, 'Antichovies Antichovies Antichovies Antichovies'),
+    ('Lettuce B. Frank', now() - '7 days'::INTERVAL, 'Lettuce B. Frank Lettuce B. Frank Lettuce B. Frank Lettuce B. Frank'),
+    ('Pepperphony', now() - '6 days'::INTERVAL, 'Pepperphony Pepperphony Pepperphony Pepperphony'),
+    ('Shamburger', now() - '5 days'::INTERVAL, 'Shamburger Shamburger Shamburger Shamburger'),
+    ('Facon', now() - '4 days'::INTERVAL, 'Facon Facon Facon Facon'),
+    ('Salami-get-this-straight', now() - '3 days'::INTERVAL, 'Salami-get-this-straight Salami-get-this-straight Salami-get-this-straight Salami-get-this-straight'),
+    ('Mi-steak', now() - '2 days'::INTERVAL, 'Mi-steak Mi-steak Mi-steak Mi-steak')
+;
